5c832a4c0806c94f6a44f2cac8ec690c08ab567e
osjjames <osj.james@gmail.com>
2019-07-12 17:57:15 +0100

    Add extra tests

--- index.test.js
+++ index.test.js
@@ -59,4 +59,41 @@ it('Add webkit prefixed props below all flexbox props', function() {
   )
 })
 
+it('Add "display: -webkit-flex" and another prop in the same rule', function() {
+  return run(
+    `a {
+      display: flex;
+      flex-direction: row-reverse;
+    }`, 
+    `a {
+      display: flex;
+      display: -webkit-flex;
+      flex-direction: row-reverse;
+      -webkit-flex-direction: row-reverse;
+    }`
+  )
+})
+
+it('Add webkit prop among other random props', function() {
+  return run(
+    `a {
+      -ms-flex-negative: 0;
+      flex-shrink: 0;
+      width: 100%;
+      height: 100%;
+      position: relative;
+      transition-property: transform;
+    }`, 
+    `a {
+      -ms-flex-negative: 0;
+      flex-shrink: 0;
+      -webkit-flex-shrink: 0;
+      width: 100%;
+      height: 100%;
+      position: relative;
+      transition-property: transform;
+    }`
+  )
+})
+
 
